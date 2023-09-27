9b20c46a841ce5a094b4880d659853c3d2c7ceb2
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-09-27 11:53:01 -0300

    Create test\GrafoTest.java

---
+++ codigo/test\GrafoTest.java
@@ -0,0 +1,17 @@
+public class GrafoTest {
+
+    @Test
+    public void testExisteEstrada() {
+        Grafo grafo = new Grafo();
+        Cidade cidadeA = new Cidade(1, "Cidade A");
+        Cidade cidadeB = new Cidade(2, "Cidade B");
+        Estrada estradaAB = new Estrada(cidadeA, cidadeB, 100);
+
+        grafo.adicionarCidade(cidadeA);
+        grafo.adicionarCidade(cidadeB);
+        grafo.adicionarEstrada(estradaAB);
+
+        assertTrue(grafo.existeEstrada(cidadeA, cidadeB));
+        assertFalse(grafo.existeEstrada(cidadeB, cidadeA));
+    }
+}
