3f1b0c750888f8ff594b1350712e7d90751aa1be
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-10-11 10:36:38 -0300

    Update Main.java

--- codigo/main/Main.java
+++ codigo/main/Main.java
@@ -23,63 +23,22 @@ public class Main {
             
             switch (opcao) {
                 case 1:
-                    System.out.print("Informe o ID da cidade: ");
-                    int cidadeId = scanner.nextInt();
-                    scanner.nextLine();  // Consumir a quebra de linha
-                    System.out.print("Informe o nome da cidade: ");
-                    String cidadeNome = scanner.nextLine();
-                    Cidade cidade = new Cidade(cidadeId, cidadeNome);
-                    grafo.adicionarCidade(cidade);
-                    System.out.println("Cidade adicionada com sucesso!");
+                    
                     break;
                 case 2:
-                    System.out.print("Informe o ID da cidade de origem: ");
-                    int origemId = scanner.nextInt();
-                    System.out.print("Informe o ID da cidade de destino: ");
-                    int destinoId = scanner.nextInt();
-                    System.out.print("Informe o peso da estrada: ");
-                    int peso = scanner.nextInt();
-                    Cidade origem = buscarCidadePorId(grafo, origemId);
-                    Cidade destino = buscarCidadePorId(grafo, destinoId);
-                    if (origem != null && destino != null) {
-                        Estrada estrada = new Estrada(origem, destino, peso);
-                        grafo.adicionarEstrada(estrada);
-                        System.out.println("Estrada adicionada com sucesso!");
-                    } else {
-                        System.out.println("Cidade de origem ou destino não encontrada.");
-                    }
+                    
                     break;
                 case 3:
-                    System.out.print("Informe o ID da cidade de origem: ");
-                    int origemIdVerificar = scanner.nextInt();
-                    System.out.print("Informe o ID da cidade de destino: ");
-                    int destinoIdVerificar = scanner.nextInt();
-                    Cidade origemVerificar = buscarCidadePorId(grafo, origemIdVerificar);
-                    Cidade destinoVerificar = buscarCidadePorId(grafo, destinoIdVerificar);
-                    if (origemVerificar != null && destinoVerificar != null) {
-                        if (grafo.existeEstrada(origemVerificar, destinoVerificar)) {
-                            System.out.println("Existe uma estrada entre as cidades.");
-                        } else {
-                            System.out.println("Não existe uma estrada entre as cidades.");
-                        }
-                    } else {
-                        System.out.println("Cidade de origem ou destino não encontrada.");
-                    }
+                    
                     break;
                 case 4:
-                    List<Cidade> inalcancaveis = grafo.cidadesInalcancaveis();
-                    System.out.println("Cidades inalcançáveis:");
-                    for (Cidade inalcancavel : inalcancaveis) {
-                        System.out.println(inalcancavel.obterNome());
-                    }
+                    
                     break;
                 case 5:
-                    System.out.println("Recomendação de Visitação em Todas as Cidades e Estradas:");
-                    // Implemente a lógica para recomendar a visitação em todas as cidades e estradas aqui.
+                    
                     break;
                 case 6:
-                    System.out.println("Recomendação de Rota de Menor Distância:");
-                    // Implemente a lógica para recomendar a rota de menor distância aqui.
+                    
                     break;
                 case 7:
                     System.out.println("Saindo do programa.");
@@ -90,13 +49,5 @@ public class Main {
             }
         }
     }
-    
-    private static Cidade buscarCidadePorId(Grafo grafo, int cidadeId) {
-        for (Cidade cidade : grafo.getCidades()) {
-            if (cidade.obterId() == cidadeId) {
-                return cidade;
-            }
-        }
-        return null;
-    }
+
 }
