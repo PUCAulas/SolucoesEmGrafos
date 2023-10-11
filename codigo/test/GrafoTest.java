4b4845f5bc8a58182b33470dc7d5c5c6f985363c
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-10-10 23:01:02 -0300

    Update GrafoTest.java

--- codigo/test/GrafoTest.java
+++ codigo/test/GrafoTest.java
@@ -1,21 +1,50 @@
 package test.java;
 
-import org.junit.Test;
+import main.java.Cidade;
+import main.java.Estrada;
+import main.java.Grafo;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-public class GrafoTest {
-	@Test
-    public void testExisteEstrada() {
-        Grafo grafo = new Grafo();
-        Cidade cidadeA = new Cidade(1, "Cidade A");
-        Cidade cidadeB = new Cidade(2, "Cidade B");
-        Estrada estradaAB = new Estrada(cidadeA, cidadeB, 100);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+public class GrafoTest {
+    private Grafo grafo;
+    private Cidade cidadeA;
+    private Cidade cidadeB;
+    private Cidade cidadeC;
+
+    @Before
+    public void setUp() {
+        grafo = new Grafo();
+        cidadeA = new Cidade(1, "Cidade A");
+        cidadeB = new Cidade(2, "Cidade B");
+        cidadeC = new Cidade(3, "Cidade C");
         grafo.adicionarCidade(cidadeA);
         grafo.adicionarCidade(cidadeB);
-        grafo.adicionarEstrada(estradaAB);
+        grafo.adicionarCidade(cidadeC);
+        grafo.adicionarEstrada(new Estrada(cidadeA, cidadeB, 100));
+    }
 
+    @Test
+    public void testExisteEstradaExistente() {
         assertTrue(grafo.existeEstrada(cidadeA, cidadeB));
-        assertFalse(grafo.existeEstrada(cidadeB, cidadeA));
+    }
+
+    @Test
+    public void testExisteEstradaNaoExistente() {
+        assertFalse(grafo.existeEstrada(cidadeA, cidadeC));
+    }
+
+    @Test
+    public void testCidadesInalcancaveis() {
+        List<Cidade> inalcancaveis = grafo.cidadesInalcancaveis();
+        assertEquals(1, inalcancaveis.size());
+        assertEquals(cidadeC, inalcancaveis.get(0));
     }
 }
