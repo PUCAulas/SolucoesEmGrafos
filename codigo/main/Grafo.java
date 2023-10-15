deaaa0048b40b70502f5967a6fa85a62a3d4ff18
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-10-15 17:22:01 -0300

    Update Grafo.java

--- codigo/main/Grafo.java
+++ codigo/main/Grafo.java
@@ -1,71 +1,138 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
-public class Grafo {
-    private List<Cidade> cidades;
-    private List<Estrada> estradas;
+public class Main {
+    public static void main(String[] args) {
+        Grafo grafo = lerGrafoDeArquivo("grafos.txt");
+        Scanner scanner = new Scanner(System.in);
 
-    public Grafo() {
-        this.cidades = new ArrayList<>();
-        this.estradas = new ArrayList<>();
-    }
+        int escolha;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Verificar Existência de Estrada");
+            System.out.println("2. Cidades Inalcançáveis");
+            System.out.println("3. Recomendação de Visitação em Todas as Cidades e Estradas");
+            System.out.println("4. Recomendação de Rota de Menor Distância");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+            escolha = scanner.nextInt();
 
-    public void adicionarCidade(Cidade cidade) {
-        cidades.add(cidade);
-    }
-
-    public void adicionarEstrada(Estrada estrada) {
-        estradas.add(estrada);
-    }
-
-    public boolean existeEstrada(Cidade cidadeOrigem, Cidade cidadeDestino) {
-        for (Estrada estrada : estradas) {
-            if (estrada.obterOrigem() == cidadeOrigem && estrada.obterDestino() == cidadeDestino) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public List<Cidade> cidadesInalcancaveis() {
-        List<Cidade> inalcancaveis = new ArrayList<>();
-        for (Cidade cidade : cidades) {
-            boolean alcancaOutraCidade = false;
-            for (Cidade outraCidade : cidades) {
-                if (!cidade.equals(outraCidade) && existeEstrada(cidade, outraCidade)) {
-                    alcancaOutraCidade = true;
+            switch (escolha) {
+                case 1:
+                    verificarExistenciaDeEstrada(grafo, scanner);
                     break;
+                case 2:
+                    listarCidadesInalcançaveis(grafo);
+                    break;
+                case 3:
+                    recomendarVisitação(grafo);
+                    break;
+                case 4:
+                    recomendarRotaDeMenorDistancia(grafo, scanner);
+                    break;
+                case 5:
+                    System.out.println("Saindo...\n");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.\n");
+            }
+         
+            System.out.print("\nPressione Enter para continuar...\n");
+            scanner.nextLine(); 
+            scanner.nextLine(); 
+        } while (escolha != 5);
+    }
+
+    private static Grafo lerGrafoDeArquivo(String nomeArquivo) {
+        Grafo grafo = new Grafo();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                String[] partes = linha.split(": ");
+                String nomeCidade = partes[0];
+                String[] estradas = partes[1].split(", ");
+                Cidade cidade = new Cidade(grafo.getCidades().size() + 1, nomeCidade);
+                grafo.adicionarCidade(cidade);
+
+                for (String estradaStr : estradas) {
+                    String[] estradaParts = estradaStr.split(" \\(");
+                    String cidadeDestinoNome = estradaParts[0];
+                    int peso = Integer.parseInt(estradaParts[1].replaceAll("\\D", ""));
+                    Cidade cidadeDestino = grafo.getCidadePorNome(cidadeDestinoNome);
+                    if (cidadeDestino != null) {
+                        grafo.adicionarEstrada(new Estrada(cidade, cidadeDestino, peso));
+                    }
                 }
             }
-            if (!alcancaOutraCidade) {
-                inalcancaveis.add(cidade);
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return inalcancaveis;
-    }
-    
-    public List<String> recomendarVisitas() {
-        List<String> recomendacoes = new ArrayList<>();
-        
-        for (Cidade cidade : cidades) {
-            recomendacoes.add("Visite a cidade " + cidade.obterNome());
-        }
-        
-        // Recomendar viagens ao longo das estradas
-        for (Estrada estrada : estradas) {
-            recomendacoes.add("Viaje de " + estrada.obterOrigem().obterNome() + " para " +
-                estrada.obterDestino().obterNome());
-        }
-        
-        return recomendacoes;
+
+        return grafo;
     }
 
-    public List<Cidade> menorRota(Cidade partida) {
-    	//Caixeiro viajante usando Dijkstra (inicia todo mundo com infinito) e ciclo Hamiltoniano 
-    	//Verificar se o grafo tem um ciclo. Se não tem ciclo, retornar mensagem dizendo que não é possível. Se tiver ciclo, identificar a solução que tem menor peso.
-		return null;
+    private static void verificarExistenciaDeEstrada(Grafo grafo, Scanner scanner) {
+        System.out.print("\nDigite o nome da cidade de origem: ");
+        String origemNome = scanner.next();
+        System.out.print("Digite o nome da cidade de destino: ");
+        String destinoNome = scanner.next();
+
+        Cidade origem = grafo.getCidadePorNome(origemNome);
+        Cidade destino = grafo.getCidadePorNome(destinoNome);
+
+        if (origem != null && destino != null) {
+            if (grafo.existeEstrada(origem, destino)) {
+                System.out.println("\nExiste uma estrada entre " + origemNome + " e " + destinoNome + "\n");
+            } else {
+                System.out.println("\nNão existe uma estrada entre " + origemNome + " e " + destinoNome);
+            }
+        } else {
+            System.out.println("\nCidades não encontradas.");
+        }
     }
-   
+
+    private static void listarCidadesInalcançaveis(Grafo grafo) {
+        List<Cidade> inalcancaveis = grafo.cidadesInalcancaveis();
+
+        if (inalcancaveis.isEmpty()) {
+            System.out.println("\nTodas as cidades são alcançáveis a partir de outras cidades.");
+        } else {
+            System.out.println("\nCidades inalcançáveis:");
+            for (Cidade cidade : inalcancaveis) {
+                System.out.println(cidade.obterNome());
+            }
+        }
+    }
+
+    private static void recomendarVisitação(Grafo grafo) {
+        List<String> recomendacoes = grafo.recomendarVisitas();
+        for (String recomendacao : recomendacoes) {
+            System.out.println(recomendacao);
+        }
+    }
+
+    private static void recomendarRotaDeMenorDistancia(Grafo grafo, Scanner scanner) {
+        System.out.print("\nDigite o nome da cidade de partida: ");
+        String partidaNome = scanner.next();
+        Cidade partida = grafo.getCidadePorNome(partidaNome);
+
+        if (partida != null) {
+            List<Cidade> menorRota = grafo.menorRota(partida);
+            if (menorRota != null) {
+                System.out.println("\nRota de menor distância a partir de " + partidaNome + ":");
+                for (Cidade cidade : menorRota) {
+                    System.out.println(cidade.obterNome());
+                }
+            } else {
+                System.out.println("\nNão foi possível encontrar uma rota de menor distância.");
+            }
+        } else {
+            System.out.println("\nCidade de partida não encontrada.");
+        }
+    }
 }
